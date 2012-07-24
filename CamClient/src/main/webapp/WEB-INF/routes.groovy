3cfd4e273648d2ce445f3d338d7a3ff7b05a34d6
Oscar Ivan <oscar@angellore.com.mx>
2012-07-23 23:39:16 -0500

    Added whatismyip page to let clients to know the external ip

--- CamClient/src/main/webapp/WEB-INF/routes.groovy
+++ CamClient/src/main/webapp/WEB-INF/routes.groovy
@@ -2,6 +2,7 @@
 get "/", forward: "/WEB-INF/pages/index.gtpl"
 get "/pingCam", forward: "/pingCam.groovy"
 get "/verDetalle", forward: "/verDetalle.groovy"
+get "/whatismyip", forward: "/whatismyip.groovy"
 
 get "/favicon.ico", redirect: "/images/gaelyk-small-favicon.png"
 jabber to: "/receiveJabber.groovy"
