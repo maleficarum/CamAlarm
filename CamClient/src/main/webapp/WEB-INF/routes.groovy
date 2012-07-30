ceace0e17bef925a456ca5069ff7548f1f14a8b6
Oscar Ivan Hernandez Ventura <oscar@angellore.com.mx>
2012-07-29 19:40:26 -0500

    Created new route to post images

--- CamClient/src/main/webapp/WEB-INF/routes.groovy
+++ CamClient/src/main/webapp/WEB-INF/routes.groovy
@@ -2,6 +2,8 @@
 get "/", forward: "/WEB-INF/pages/index.gtpl"
 get "/pingCam", forward: "/pingCam.groovy"
 get "/verDetalle", forward: "/verDetalle.groovy"
+post '/postImage', forward:  "/postImage.groovy"
+get '/postImage', forward:  "/postImage.groovy"
 
 get "/favicon.ico", redirect: "/images/gaelyk-small-favicon.png"
 jabber to: "/receiveJabber.groovy"
